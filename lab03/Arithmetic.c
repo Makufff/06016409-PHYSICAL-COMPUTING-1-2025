1e1e6ecd01bfa55032cf4d7386a880ff31561996
makufff <Tanapatx.tyran@gmail.com>
2025-07-16 11:17:11 +0700

    Lab03 Arithmetic.c

---
+++ lab03/Arithmetic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+	double x , y ; scanf("%lf,%lf" , &x , &y) ;
+	printf("The sum of the given numbers : %lf\nThe difference of the given numbers : %lf\nThe product of the given numbers : %lf\nThe quotient of the given numbers : %lf" , x+y , x-y , x*y , x/y) ;
+	return 0;
+}
